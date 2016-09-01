aab9ed4f54137388881a000583b88a67813e47cf
sqhan <sqhan@Ctrip.com>
2016-09-01 19:48:33 +0800

    复制功能完成

--- app/src/main/java/com/example/sqhan/myapplicationtest/MainActivity.java
+++ app/src/main/java/com/example/sqhan/myapplicationtest/MainActivity.java
@@ -1,18 +1,25 @@
 package com.example.sqhan.myapplicationtest;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Context mContext;
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textView = (TextView)findViewById(R.id.text);
+        textView = (TextView)findViewById(R.id.text);
         textView.setOnCreateContextMenuListener(new View.OnCreateContextMenuListener() {
             @Override
             public void onCreateContextMenu(ContextMenu contextMenu, View view, ContextMenu.ContextMenuInfo contextMenuInfo) {
@@ -20,5 +27,27 @@ public class MainActivity extends AppCompatActivity {
                 contextMenu.add(0,1,0,"删除");
             }
         });
+        mContext = getApplicationContext();
+
+
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)item.getMenuInfo();
+        switch (item.getItemId()){
+            case 0:{
+                String text = textView.getText().toString();
+                ClipboardManager clipboardManager = (ClipboardManager)mContext.getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData myClip = ClipData.newPlainText("可见哦",text);
+                clipboardManager.setPrimaryClip(myClip);
+                break;
+            }
+            case 1:{
+                break;
+            }
+        }
+
+        return super.onContextItemSelected(item);
     }
 }
